baaaecb0314032005a5a87d838e6951e8e699d67
mguida22 <michael.guida@colorado.edu>
2015-11-18 16:54:59 -0700

    read and display data from bluetooth

--- project/jeeves/jeeves.ino
+++ project/jeeves/jeeves.ino
@@ -5,8 +5,16 @@ void setup()
   sparki.clearLCD();
   sparki.print("Welcome to Jeeves!");
   sparki.updateLCD();
+
+  Serial1.begin(9600);
 }
 
 void loop()
 {
+  if (Serial1.available())
+  {
+    int inByte = Serial1.read();
+    sparki.print((char) inByte);
+    sparki.updateLCD();
+  }
 }
